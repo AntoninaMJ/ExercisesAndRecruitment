bf5b63dac137b8d870345118b3986180c6e2a868
Antonina <amarikhina@gmail.com>
2020-01-12 08:47:42 +0100

    added recruitment tests 2019

---
+++ src/main/recruitmentTests/Singleton.java
@@ -0,0 +1,23 @@
+package main.recruitmentTests;
+
+//singleton pattern - only one instance
+public class Singleton {
+
+    private static Singleton INSTANCE = null;
+
+    //double-check-locking pattern (2 ifs)
+    public static Singleton getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) {
+                if (INSTANCE == null) {
+                    //lazy loading - new (only one) instance after start method getInstance
+                    INSTANCE = new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private Singleton() {
+    }
+}
---
+++ src/main/recruitmentTests/Threads.java
@@ -0,0 +1,20 @@
+package main.recruitmentTests;
+
+public class Threads {
+
+    public static void main(String[] args) {
+        System.out.println("MT start");
+        Thread thread = new Thread(() -> {
+            System.out.println("T0 start");
+            for (int i = 0; i < 5; i++) {
+                System.out.println("T0 " + i);
+            }
+            System.out.println("T0 stop");
+        });
+        thread.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("MT " + i);
+        }
+        System.out.println("MT stop");
+    }
+}
