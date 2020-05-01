400600a95eec8f52cb1b940cd80ced35ad60bbf9
Antonina <amarikhina@gmail.com>
2020-05-01 21:31:46 +0200

    Added practice test (HackerRank - StdinAndStdout)

---
+++ src/main/hackerRank/StdinAndStdout.java
@@ -0,0 +1,24 @@
+package main.hackerRank;
+
+import java.util.Scanner;
+
+public class StdinAndStdout {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+//42
+//3.1415
+//Welcome to HackerRank's Java tutorials!
