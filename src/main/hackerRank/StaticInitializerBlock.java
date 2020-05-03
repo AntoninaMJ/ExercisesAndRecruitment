bc3d9d38a7919acc389a2c57217c3434d82e2d1d
Antonina <amarikhina@gmail.com>
2020-05-03 13:18:25 +0200

    Added practice test (HackerRank - StaticInitializerBlock)

---
+++ src/main/hackerRank/StaticInitializerBlock.java
@@ -0,0 +1,23 @@
+package main.hackerRank;
+
+import java.util.Scanner;
+
+public class StaticInitializerBlock {
+    private static int B, H;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int area = B * H;
+        System.out.print(area);
+    }
+}
